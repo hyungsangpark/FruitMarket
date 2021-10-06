e2c4149a4a1a486b2ba3d4ae6f697dc21158456f
alieeveely <yuwei.s@hotmail.co.uk>
2021-10-06 22:23:06 +1300

    Updated abstract class with planned fields and methods

--- app/src/main/java/com/example/fruitmarket/Fruit.java
+++ app/src/main/java/com/example/fruitmarket/Fruit.java
@@ -1,4 +1,53 @@
 package com.example.fruitmarket;
 
+import java.util.ArrayList;
+
 abstract class Fruit implements IProduct{
+    private long id;
+    private String name;
+    private String category;
+    private String producer;
+    private ArrayList<String> images;
+    private String variety;
+    private String[] description;
+    private float price;
+    private PriceMetric priceMetric;
+    private Boolean inStock;
+
+    public String getName(){
+        return name;
+    };
+
+    public String getCategory(){
+        return category;
+    }
+
+    public String getProducer(){
+        return producer;
+    }
+
+    public ArrayList<String> getImages(){
+        return images;
+    }
+
+    public String getVariety(){
+        return variety;
+    }
+
+    public String[] getDescription(){
+        return description;
+    }
+
+    public float getPrice(){
+        return price;
+    }
+
+    public PriceMetric getPriceMetric(){
+        return priceMetric;
+    }
+
+    public Boolean getInStock(){
+        return inStock;
+    }
+
 }
