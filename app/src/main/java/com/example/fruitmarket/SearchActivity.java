3f2fb0a6246c0fae907660cfc312725b6fe5ea67
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-10-11 23:50:29 +1300

    Fixed Back Button, Added Voice Input, and added category buttons.

--- app/src/main/java/com/example/fruitmarket/SearchActivity.java
+++ app/src/main/java/com/example/fruitmarket/SearchActivity.java
@@ -1,40 +1,101 @@
 package com.example.fruitmarket;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.fruitmarket.adaptors.CategoryFilterAdaptor;
-import com.example.fruitmarket.data.DataProvider;
+import com.example.fruitmarket.adaptors.SearchAutoCompleteAdaptor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class SearchActivity extends AppCompatActivity {
 
-    List<String> categories;
-    CategoryFilterAdaptor adapter;
-    RecyclerView rvButtons;
+    private EditText searchEditText;
+    private ImageButton searchByVoiceIcon;
+    private List<String> categories;
+    private CategoryFilterAdaptor categoryFilterButtonsAdapter;
+    private RecyclerView rvButtons;
+    private ListView searchSuggestions;
+    private SearchAutoCompleteAdaptor searchSuggestionsAdaptor;
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        rvButtons = (RecyclerView) findViewById(R.id.category_filter_recycler_view);
+        // Enable Tool bar to work.
+        setSupportActionBar(findViewById(R.id.search_tool_bar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        categories = DataProvider.generateFruitsList();
-        adapter = new CategoryFilterAdaptor(categories);
-        rvButtons.setAdapter(adapter);
+        // Configure Speech Recognition components.
+        searchEditText = findViewById(R.id.search_edit_text);
+        searchByVoiceIcon = findViewById(R.id.search_by_voice_icon);
+        searchByVoiceIcon.setOnClickListener((View view) -> {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                    Locale.ENGLISH.toString());
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
 
-        rvButtons.addItemDecoration(adapter.new MarginItemDecoration(
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+            } catch (Exception e) {
+                Toast
+                        .makeText(SearchActivity.this, "" + e.getMessage(),
+                                Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        rvButtons = findViewById(R.id.category_filter_recycler_view);
+        // TODO: Modify this array to import from a legitimate source of these categories.
+        categories = Arrays.asList("Kiwifruit", "Apple", "Orange", "Blueberry", "Feijoa");
+        categoryFilterButtonsAdapter = new CategoryFilterAdaptor(categories);
+        rvButtons.setAdapter(categoryFilterButtonsAdapter);
+        rvButtons.addItemDecoration(categoryFilterButtonsAdapter.new MarginItemDecoration(
                 (int) getResources().getDimension(R.dimen.category_filter_button_horizontal_margin),
                 (int) getResources().getDimension(R.dimen.category_filter_button_vertical_margin)));
-
         LinearLayoutManager lm = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-
         rvButtons.setLayoutManager(lm);
+
+        searchSuggestions = findViewById(R.id.search_suggestions_list_view);
+//        searchSuggestionsAdaptor = new SearchAutoCompleteAdaptor();
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                searchEditText.setText(Objects.requireNonNull(result).get(0));
+            }
+        }
     }
 }
