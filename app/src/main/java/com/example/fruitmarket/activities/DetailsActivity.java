5f31021973202814d9e9816db6c519ea19b22de4
hiin3d55 <dave.rotoz@gmail.com>
2021-10-20 18:11:58 +1300

    Recover the files that were lost from merge conflict. implementDetailsActivityDave -> main.

--- app/src/main/java/com/example/fruitmarket/activities/DetailsActivity.java
+++ app/src/main/java/com/example/fruitmarket/activities/DetailsActivity.java
@@ -12,30 +12,48 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.viewpager.widget.ViewPager;
 
-import com.example.fruitmarket.R;
+import com.example.fruitmarket.adapters.ViewPagerAdapter;
 
 public class DetailsActivity extends AppCompatActivity {
     class ViewHolder {
-        EditText priceEditText, usernameEditText;
-        TextView totalOrderTextView, quantityTextView;
-        CardView cardViewResults;
-        Button confirmButton;
+        TextView fruitNameTextView, producerTextView, descriptionTextView, priceTextView,
+                availabilityTextView;
     }
 
-    ViewHolder vh;
+    DetailsActivity.ViewHolder vh;
+
+    // creating object of ViewPager
+    ViewPager mViewPager;
+
+    // images array
+    int[] images = {R.drawable.a1, R.drawable.a2, R.drawable.a3};
+
+    // Creating Object of ViewPagerAdapter
+    ViewPagerAdapter mViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-//        vh = new ViewHolder();
-//        vh.priceEditText = (EditText) findViewById(R.id.price_edit_text);
-//        vh.totalOrderTextView = (TextView) findViewById(R.id.total_order_text_view);
-//        vh.quantityTextView = findViewById(R.id.quantity_text_view);
-//        vh.cardViewResults = (CardView) findViewById(R.id.card_view_result_message);
-//        vh.usernameEditText = (EditText) findViewById(R.id.edit_text_username);
-//        vh.confirmButton= (Button) findViewById(R.id.confirm_button);
+        vh = new DetailsActivity.ViewHolder();
+        vh.fruitNameTextView = (TextView) findViewById(R.id.fruit_name_text_view);
+        vh.producerTextView = (TextView) findViewById(R.id.producer_text_view);
+        vh.descriptionTextView = (TextView) findViewById(R.id.description_text_view);
+        vh.priceTextView = (TextView) findViewById(R.id.price_text_view);
+        vh.availabilityTextView = (TextView) findViewById(R.id.availability_text_view);
+
+        vh.fruitNameTextView.setText("Implement the rest when DataProvider is finished");
+
+        // Initializing the ViewPager Object
+        mViewPager = (ViewPager)findViewById(R.id.imagesSlider);
+
+        // Initializing the ViewPagerAdapter
+        mViewPagerAdapter = new ViewPagerAdapter(DetailsActivity.this, images);
+
+        // Adding the Adapter to the ViewPager
+        mViewPager.setAdapter(mViewPagerAdapter);
     }
 }
