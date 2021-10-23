2ab515ac4625dfe6e7ccd3e6c327465c99b73cf5
hyungsangpark <hpar461@aucklanduni.ac.nz>
2021-10-23 16:29:27 +1300

    Fixed updating popularity updating the whole document in the firestore.

--- app/src/main/java/com/example/fruitmarket/activities/DetailsActivity.java
+++ app/src/main/java/com/example/fruitmarket/activities/DetailsActivity.java
@@ -62,12 +62,12 @@ public class DetailsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        fruit = (IProduct)getIntent().getSerializableExtra("IProduct");
+        fruit = (IProduct) getIntent().getSerializableExtra("IProduct");
         fruit.incrementPopularity();
-        DataProvider.updatePopularityToFirestore(fruit);
+        DataProvider.getInstance().updatePopularityToFirestore(fruit);
 
         for (int i = 0; i < 3; i++) {
-            String imageName =  fruit.getImages().get(i).split("\\.")[0];
+            String imageName = fruit.getImages().get(i).split("\\.")[0];
             images[i] = DetailsActivity.this.getResources().getIdentifier(
                     imageName, "drawable",
                     DetailsActivity.this.getPackageName());
@@ -86,7 +86,7 @@ public class DetailsActivity extends AppCompatActivity {
         setColour(fruit, vh.nameContainer, vh.tabs);
 
         // Initializing the ViewPager Object
-        mViewPager = (ViewPager)findViewById(R.id.imagesSlider);
+        mViewPager = (ViewPager) findViewById(R.id.imagesSlider);
 
         // Initializing the ViewPagerAdapter
         mViewPagerAdapter = new ViewPagerAdapter(DetailsActivity.this, images);
@@ -110,40 +110,40 @@ public class DetailsActivity extends AppCompatActivity {
     }
 
     private void setColour(IProduct fruit, LinearLayout element, TabLayout layout) {
-        switch(fruit.getCategory()) {
+        switch (fruit.getCategory()) {
             case "Apple":
                 element.setBackgroundColor(ResourcesCompat.getColor(getResources(),
-                                           R.color.apples_dark, null));
+                        R.color.apples_dark, null));
                 layout.setSelectedTabIndicatorColor(ResourcesCompat.getColor(getResources(),
                         R.color.apples_dark, null));
                 break;
-            case  "Blueberry":
+            case "Blueberry":
                 element.setBackgroundColor(ResourcesCompat.getColor(getResources(),
-                                           R.color.blueberries_dark, null));
+                        R.color.blueberries_dark, null));
                 layout.setSelectedTabIndicatorColor(ResourcesCompat.getColor(getResources(),
                         R.color.blueberries_dark, null));
                 break;
             case "Feijoa":
                 element.setBackgroundColor(ResourcesCompat.getColor(getResources(),
-                                           R.color.feijoas_dark, null));
+                        R.color.feijoas_dark, null));
                 layout.setSelectedTabIndicatorColor(ResourcesCompat.getColor(getResources(),
                         R.color.feijoas_dark, null));
                 break;
             case "Kiwifruit":
                 element.setBackgroundColor(ResourcesCompat.getColor(getResources(),
-                                           R.color.kiwifruits_dark, null));
+                        R.color.kiwifruits_dark, null));
                 layout.setSelectedTabIndicatorColor(ResourcesCompat.getColor(getResources(),
                         R.color.kiwifruits_dark, null));
                 break;
             case "Orange":
                 element.setBackgroundColor(ResourcesCompat.getColor(getResources(),
-                                           R.color.oranges_dark, null));
+                        R.color.oranges_dark, null));
                 layout.setSelectedTabIndicatorColor(ResourcesCompat.getColor(getResources(),
                         R.color.oranges_dark, null));
                 break;
             default:
                 element.setBackgroundColor(ResourcesCompat.getColor(getResources(),
-                                           R.color.purple_500, null));
+                        R.color.purple_500, null));
                 layout.setSelectedTabIndicatorColor(ResourcesCompat.getColor(getResources(),
                         R.color.purple_500, null));
         }
